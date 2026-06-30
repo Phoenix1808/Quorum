bab5eafb2427afc63d45ef47d1fa98981ea4a64d
Divyansh Goyal <goyaldivyansh1111@gmail.com>
2026-07-01 03:14:28 +0530

    added wallet connection address visibility in real time along with disconnection

--- android/app/src/main/java/com/example/quorum/ui/wallet/WalletScreen.kt
+++ android/app/src/main/java/com/example/quorum/ui/wallet/WalletScreen.kt
@@ -16,6 +16,7 @@ import com.reown.appkit.ui.components.button.AppKitState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import com.reown.appkit.client.AppKit
 
 // TODO: WalletScreen — connect button + connected state
 
@@ -35,7 +36,20 @@ fun WalletScreen(
         Spacer(Modifier.height(16.dp))
 
         if(isConnect){
+            val acc = AppKit.getAccount()
+            val address = acc?.address ?: "--"
             Text("Wallet Connected", style = MaterialTheme.typography.titleMedium)
+
+            val shortAdd = if (address.length>10) "${address.take(6)}..${address.takeLast(4)}" //like take 6 chars from beginning and 4 from ending
+            else address
+            Text(shortAdd,style=MaterialTheme.typography.bodyMedium)
+
+            Spacer(Modifier.height(16.dp))
+
+            Button(onClick = {AppKit.disconnect(onSuccess = {}, onError = {})
+            }) {
+                Text("Disconnect")
+            }
         } else{
             Text("Vote by connecting your Wallet", style= MaterialTheme.typography.bodyMedium)
             Spacer(Modifier.height(16.dp))
